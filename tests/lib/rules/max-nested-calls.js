83ee855232266ad4d27d31c503b93681ed278d49
Victor Nakoryakov <nail.xx@gmail.com>
2017-01-20 15:42:36 +0300

    feat: support plain integer option

--- tests/lib/rules/max-nested-calls.js
+++ tests/lib/rules/max-nested-calls.js
@@ -22,7 +22,7 @@ ruleTester.run("max-nested-calls", rule, {
   valid: [
     { code: 'foo(bar())' },
     { code: 'foo(bar(baz()))', options: [{ max: 3 }] },
-    { code: 'foo(bar(baz()), qux())', options: [{ max: 3 }] }
+    { code: 'foo(bar(baz()), qux())', options: [ 3 ] }
   ],
 
   invalid: [
