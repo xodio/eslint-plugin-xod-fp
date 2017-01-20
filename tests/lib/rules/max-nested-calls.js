62a78244f287852edf504aba81e926a982c2df02
Victor Nakoryakov <nail.xx@gmail.com>
2017-01-20 19:48:37 +0300

    fix(max-nested-calls): handle chain calls correctly

--- tests/lib/rules/max-nested-calls.js
+++ tests/lib/rules/max-nested-calls.js
@@ -11,6 +11,38 @@
 const rule = require("../../../lib/rules/max-nested-calls");
 const RuleTester = require("eslint").RuleTester;
 
+const parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+//------------------------------------------------------------------------------
+// Fixtures
+//------------------------------------------------------------------------------
+
+const fixture = [
+  'const convertPatches = R.compose(',
+  '  composeT,',
+  '  R.converge(',
+  '    Tuple,',
+  '    [',
+  '      R.compose(R.mergeAll, R.pluck(0)),',
+  '      R.compose(composeA, R.pluck(1)),',
+  '    ]',
+  '  ),',
+  '  R.map(oldPatch => Maybe.of(Patch.createPatch())',
+  '    .chain(apOrSkip(addLabel(oldPatch)))',
+  '    .chain(apOrSkip(copyImpls(oldPatch)))',
+  '    .map(convertPatchPins(oldPatch))',
+  '    .chain(convertNodes(oldPatch))',
+  '    .map(assocPatchUnsafe(oldPatch.id))',
+  '  ),',
+  '  R.values,',
+  '  mergePatchesAndNodeTypes',
+  ');'
+].join('\n');
 
 //------------------------------------------------------------------------------
 // Tests
@@ -22,7 +54,8 @@ ruleTester.run("max-nested-calls", rule, {
   valid: [
     { code: 'foo(bar())' },
     { code: 'foo(bar(baz()))', options: [{ max: 3 }] },
-    { code: 'foo(bar(baz()), qux())', options: [ 3 ] }
+    { code: 'foo(bar(baz()), qux())', options: [ 3 ] },
+    { code: fixture, options: [ 5 ], parserOptions }
   ],
 
   invalid: [
@@ -30,6 +63,23 @@ ruleTester.run("max-nested-calls", rule, {
       code: 'foo(bar(baz()))',
       options: [{ max: 2 }],
       errors: ["Function calls are nested too deeply (3)."]
+    },
+    {
+      code: fixture,
+      options: [ 4 ],
+      errors: [
+        {
+          message: "Function calls are nested too deeply (5).",
+          line: 11,
+          column: 21
+        },
+        {
+          message: "Function calls are nested too deeply (5).",
+          line: 12,
+          column: 21
+        },
+      ],
+      parserOptions
     }
   ]
 });
